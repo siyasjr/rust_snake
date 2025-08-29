ceaa704bfa1181355759f560046202894212a6c5
siyasjr <siyasjr@gmail.com>
2025-08-29 23:58:50 +0530

    into scope

--- src/main.rs
+++ src/main.rs
@@ -4,3 +4,10 @@ extern crate opengl_graphics;
 extern crate piston;
 extern crate rand;
 
+use glutin_window::GlutinWindow as Window;
+use opengl_graphics::{GlGraphics, OpenGL};
+use crate::piston::EventLoop;
+use piston::event_loop::{EventSettings, Events};
+use piston::input::{RenderArgs, RenderEvent, UpdateArgs, UpdateEvent, ButtonArgs, ButtonEvent, Button, ButtonState, Key};
+use piston::window::WindowSettings;
+use rand::Rng;
